3927f2a1cc632077cb863ebc084053dffb13cd36
Sergey <heyyyoyy@gmail.com>
2022-11-25 19:50:10 +0300

    https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/rust

---
+++ src/mumbling.rs
@@ -0,0 +1,30 @@
+
+fn accum(s:&str)->String {
+    s
+    .to_lowercase()
+    .chars()
+    .enumerate()
+    .map(|(i, ch)| 
+        format!(
+            "{}{}-",
+            ch.to_uppercase().to_string(),
+            std::iter::repeat(ch).take(i).collect::<String>()))
+    .collect::<String>()
+    .trim_end_matches('-')
+    .to_string()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_tests() {
+      assert_eq!(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+      assert_eq!(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+      assert_eq!(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+      assert_eq!(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+      assert_eq!(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+    }
+}
